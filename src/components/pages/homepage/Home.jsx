409d635f3c311fa70054d3a8b9dc40e928e5f6f0
randyaajr <67880000+randyaajr@users.noreply.github.com>
2021-08-26 15:45:52 -0400

    Added new components

--- src/components/pages/homepage/Home.jsx
+++ src/components/pages/homepage/Home.jsx
@@ -1,12 +1,17 @@
 import Header from "../../header/Header";
+import Posts from "../../posts/Posts";
+import Sidebar from "../../sidebar/Sidebar";
 import "./Home.scss";
 
 const Home = () => {
   return (
-    <div className="home">
+    <>
       <Header />
-      Home Page
-    </div>
+      <div className="home">
+        <Posts />
+        <Sidebar />
+      </div>
+    </>
   );
 };
 
