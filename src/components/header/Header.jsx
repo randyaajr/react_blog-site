3e9b6bf2cb4f8f86fadbc6ecf4bd81aea34881cd
randyaajr <67880000+randyaajr@users.noreply.github.com>
2021-08-26 14:56:40 -0400

    Created header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,19 @@
-import React from "react";
 import "./Header.scss";
 
 const Header = () => {
-  return <div></div>;
+  return (
+    <div className="header">
+      <div className="header-Title">
+        <span className="header-Title-Sm">React & Node</span>
+        <span className="header-Title-Lg">Blog</span>
+      </div>
+      <img
+        src="https://images.pexels.com/photos/5011647/pexels-photo-5011647.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+        alt=""
+        className="header-Img"
+      />
+    </div>
+  );
 };
 
 export default Header;
