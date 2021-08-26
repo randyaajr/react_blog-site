0b8df0e691e9d24512d24788300232d35e957d67
randyaajr <67880000+randyaajr@users.noreply.github.com>
2021-08-26 16:53:47 -0400

    Added new component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import "./App.scss";
 import Navbar from "./components/navbar/Navbar";
 import Home from "./components/pages/homepage/Home";
+import Singlepost from "./components/pages/singlepost/Singlepost";
 
 function App() {
   return (
     <>
       <div className="App">
         <Navbar />
-        <Home />
+        <Singlepost />
       </div>
     </>
   );
